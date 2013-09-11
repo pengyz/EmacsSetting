8a24f076b232059aad6fb2f0bcab12a2fa9ec18a
orangekbb <orangekbb@gmail.com>
2013-09-11 23:06:18 +0800

    plugin-settings changed

--- my-lisp/my-plugin-settings.el
+++ my-lisp/my-plugin-settings.el
@@ -268,17 +268,16 @@
 
 ;;Cygwin
 ;;========================================================
-(require 'cygwin-mount)
 ;; 加载cygwin 
 (setenv "PATH" (concat "D:/Cygwin/bin" (getenv "PATH"))) 
 (setq exec-path (cons "D:/Cygwin/bin" exec-path)) 
 (require 'cygwin-mount) 
 (cygwin-mount-activate) 
-
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (add-hook 'comint-output-filter-functions                                       
                     'shell-strip-ctrl-m nil t)                                                  
 (add-hook 'comint-output-filter-functions                                       
-                    'comint-watch-for-password-prompt nil t)                                    
+                   'comint-watch-for-password-prompt nil t)                                    
 (setq explicit-shell-file-name "bash.exe")                                      
 ;; For subprocesses invoked via the shell                                       
 ;; (e.g., "shell -c command")                                                   
